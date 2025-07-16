821d429f9ba470f3fc783999a03889fa3228df27
Sam Turner <sam.turner@kokadjo.net>
2025-07-16 12:19:34 -0400

    created 2 methods to read and write in http datastore tp persist cart data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { uiActions } from './store/ui-slice';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
@@ -13,52 +15,27 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-        })
-      );
-      const response = await fetch('https://react-test-2d9ac-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      }
-      );
+  useEffect(() => {
+    console.log(isInitial);
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-
-        })
-      );
-    };
-
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
   }, [cart, dispatch]);
 
   return (
     <Fragment>
-      {notification && <Notification 
-      status={notification.status}
-      title={notification.title}
-      message={notification.message} 
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
       />}
       <Layout>
         {showCart && <Cart />}
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -3,15 +3,21 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialCartState = {
     items: [],
     totalQuantity: 0,
+    changed: false,
 };
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: initialCartState,
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         AddItem(state, action) {
             const itemToAdd = action.payload;
             state.totalQuantity++;
+            state.changed = true;
             const existingItem = state.items.find(item => item.id == itemToAdd.id);
             if (!existingItem) {
                 state.items.push({
@@ -29,6 +35,7 @@ const cartSlice = createSlice({
         removeItem(state, action) {
             const id = action.payload;
             state.totalQuantity--;
+            state.changed = true;
             const existingItem = state.items.find(item => item.id == id);
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
@@ -41,6 +48,8 @@ const cartSlice = createSlice({
         purchase() { }
     }
 });
+
+
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
